08967afefa306867df8aed4ae5daddb22124faf2
chido <chidodesigns@gmail.com>
2026-02-05 16:17:28 +0000

    binding services to the container

---
+++ config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Response;
+
+$routes = [
+    
+    [
+        'GET',
+        '/books/{id:\d+}',
+        fn() => new Response()
+    ]
+    
+];
+
+return $routes;
--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -47,6 +47,8 @@ abstract class ApiTestCase extends BaseTestCase
         //  Create / resolve the Kernel
         $kernel = $this->container->get(Kernel::class);
 
+        dd($kernel);
+
         
         // Obtain a $response object: $response = $kernel->handle($request)
         $response = $kernel->handle($request);
